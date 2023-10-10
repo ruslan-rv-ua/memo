f6e6559e9411fa20adb40a4987bfed0621bf0cf5
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2023-10-10 21:06:40 +0300

    chore: todos

--- memo/memo_item.py
+++ memo/memo_item.py
@@ -4,90 +4,70 @@
 HASHTAGS_LINE_PREFIX = "Hashtags: "
 
 
-class Memo:
-    """A memo."""
+# class Memo:
+#     """A memo."""
 
-    def __init__(self, markdown: str = "", hashtags=None):
-        """Initialize a new instance of the Memo class.
+#     def __init__(self, markdown: str = "", hashtags=None):
+#         """Initialize a new instance of the Memo class.
 
-        Args:
-            markdown (str): the content of the memo.
-            hashtags (list): the list of extra hashtags.
-        """
-        self._lines = markdown.strip().splitlines()
-        if hashtags:
-            self.set_hashtags(hashtags)
+#         Args:
+#             markdown (str): the content of the memo.
+#             hashtags (list): the list of extra hashtags.
+#         """
+#         if hashtags:
 
-    @property
-    def markdown(self):
-        """Get the content of the memo."""
-        return "\n".join(self._lines)
+#     @property
+#     def markdown(self):
+#         """Get the content of the memo."""
 
-    @property
-    def title(self):
-        """Get the title of the memo."""
-        if self._lines:
-            # first line without markdown syntax
-            return self._lines[0].lstrip("# ")
-        return ""
+#     @property
+#     def title(self):
+#         """Get the title of the memo."""
+#         if self._lines:
+#             # first line without markdown syntax
 
-    def _find_hashtags_line_index(self):
-        """Find the index of the line that contains only hashtags.
+#     def _find_hashtags_line_index(self):
+#         """Find the index of the line that contains only hashtags.
 
-        The line must start with HASHTAGS_LINE_PREFIX.
+#         The line must start with HASHTAGS_LINE_PREFIX.
 
-        Returns:
-            int: the index of the line that contains only hashtags or -1 if not found.
-        """
-        # beckward search
-        for i in range(len(self._lines) - 1, -1, -1):
-            if self._lines[i].startswith(HASHTAGS_LINE_PREFIX):
-                return i
-        return -1
+#         Returns:
+#             int: the index of the line that contains only hashtags or -1 if not found.
+#         """
+#         # beckward search
+#         for i in range(len(self._lines) - 1, -1, -1):
+#             if self._lines[i].startswith(HASHTAGS_LINE_PREFIX):
 
-    def has_hashtags(self):
-        """Check if the memo has hashtags.
+#     def has_hashtags(self):
+#         """Check if the memo has hashtags.
 
-        Returns:
-            bool: True if the memo has hashtags, False otherwise.
-        """
-        return self._find_hashtags_line_index() >= 0
+#         Returns:
+#             bool: True if the memo has hashtags, False otherwise.
+#         """
 
-    @property
-    def hashtags(self):
-        """Get the hashtags from the memo.
+#     @property
+#     def hashtags(self):
+#         """Get the hashtags from the memo.
 
-        Returns:
-            set: the list of hashtags.
-        """
-        index = self._find_hashtags_line_index()
-        if index >= 0:
-            # get the hashtags from the line
-            return set(self._lines[index][len(HASHTAGS_LINE_PREFIX) :].split())
-        return set()
+#         Returns:
+#             set: the list of hashtags.
+#         """
+#         if index >= 0:
+#             # get the hashtags from the line
 
-    def set_hashtags(self, hashtags):
-        """Set the hashtags of the memo.
+#     def set_hashtags(self, hashtags):
+#         """Set the hashtags of the memo.
 
-        Args:
-            hashtags: the list of hashtags.
-        """
-        # all hashtags to lowercase
-        hashtags = [f"#{hashtag.lstrip('#').lower()}" for hashtag in hashtags]
-        index = self._find_hashtags_line_index()
-        if index < 0:
-            # add new line
-            self._lines.append("")
-            self._lines.append("")
-            self._lines.append("---")
-            self._lines.append("")
-            index = len(self._lines) - 1
-        self._lines[index] = HASHTAGS_LINE_PREFIX + " ".join(sorted(hashtags))
+#         Args:
+#             hashtags: the list of hashtags.
+#         """
+#         # all hashtags to lowercase
+#         if index < 0:
+#             # add new line
 
-    def update_hashtags(self, hashtags):
-        """Update the hashtags of the memo.
+#     def update_hashtags(self, hashtags):
+#         """Update the hashtags of the memo.
 
-        Args:
-            hashtags: the list of hashtags.
-        """
-        self.set_hashtags(self.hashtags.union(set(hashtags)))
+#         Args:
+#             hashtags: the list of hashtags.
+#         """
